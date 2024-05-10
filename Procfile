c257af1a065a5001b2d8ecf3d7598a8e936ce85d
Filipp <filipp@balakin.ru>
2024-05-10 22:56:46 +0100

    Update deploy settings

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn backend.wsgi --timeout 15 --keep-alive 5 --log-level debug
-
+web: gunicorn backend.wsgi:application -c conf/gunicorn_conf.py
---
+++ conf/gunicorn.conf.py
@@ -0,0 +1,21 @@
+import os
+
+
+def post_worker_init(worker) -> None: # noqa
+    import backend.urls  # noqa
+
+
+timeout = 60
+workers = 4
+worker_class = 'gevent'
+worker_connections = 100
+graceful_timeout = 600
+forwarded_allow_ips = '*'
+secure_scheme_headers = {'X-FORWARDED-PROTO': 'https'}
+name = "finance-app"
+bind = f"0.0.0.0:{os.getenv('PORT', 8000)}"
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s %(D)s "%(f)s" "%(a)s"'
+max_requests = 100
+max_requests_jitter = 30
+limit_request_line = 8190
