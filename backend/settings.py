138081bb7297764215b58e5a847190b1195883a9
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-05-11 01:15:18 +0400

    Fixes

--- backend/settings.py
+++ backend/settings.py
@@ -169,7 +169,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'https://walletwave-079e0ff9a62d.herokuapp.com/',
     'https://walletwave.app',
-    'https://walletwave-nu.vercel.app'
+    'https://walletwave-nu.vercel.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -177,7 +177,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:3000',                     
     'https://walletwave-079e0ff9a62d.herokuapp.com/',
     'https://walletwave.app',
-    'https://walletwave-nu.vercel.app'
+    'https://walletwave-nu.vercel.app',
 
 ]
 
@@ -185,7 +185,7 @@ CORS_ORIGINS_WHITELIST = [
     'http://127.0.0.1:8000',
     'https://walletwave-079e0ff9a62d.herokuapp.com/',
     'https://walletwave-nu.vercel.app',
-    'https://walletwave.app'
+    'https://walletwave.app',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
