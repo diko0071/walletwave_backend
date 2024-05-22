20c9447ac3554485a3df8cb9dbcbd4b73324b337
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-05-22 05:19:23 +0400

    change interval to 5m

--- celery_app.py
+++ celery_app.py
@@ -14,6 +14,8 @@ app.autodiscover_tasks()
 
 app.conf.beat_scheduler = 'django_celery_beat.schedulers:DatabaseScheduler'
 
+app.conf.beat_max_loop_interval = 300
+
 @app.task(name='debug_task')
 def debug_task():
     time.sleep(10)
