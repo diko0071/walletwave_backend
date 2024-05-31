d1c46800038a2573fe9b0fa08e3d866961022523
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-06-01 03:54:16 +0400

    email integration

--- transaction/services.py
+++ transaction/services.py
@@ -1,6 +1,9 @@
 import dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
+import requests
+import os
+
 
 dotenv.load_dotenv()
 
@@ -13,3 +16,30 @@ def ai_transaction_converter(human_message, system_message):
     HumanMessage(content=human_message),
         ]
     return chat.invoke(messages)
+
+
+def email_before_reccuring_transaction(email, description, name, amount, currency):
+
+    url = "https://app.loops.so/api/v1/transactional"
+
+    if name is None:
+        name = ""
+
+    payload = {
+        "transactionalId": "clwvbrtz901k0lb4dxysn046m",
+        "email": email,
+        "dataVariables": {
+            "name": name,
+            "description": description,
+            "amount": amount,
+            "currency": currency,
+        }
+    }
+    headers = {
+        "Authorization": f"Bearer {os.getenv('LOOPS_API_KEY')}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    return response.json()
