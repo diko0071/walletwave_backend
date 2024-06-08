9300fa830a0473d07dc67b0ef3f8a3283423fb12
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-06-08 06:56:52 +0400

    allowed hosts added

--- backend/settings.py
+++ backend/settings.py
@@ -29,6 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(',')
+
 EXCHANGE_RATE_API_URL = os.getenv('EXCHANGE_RATE_API_URL')
 
 DEBUG = True 
