4a476bf196840f8329fd9ca51c5b21f92bbb099d
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-06-08 06:34:35 +0400

    debug

--- backend/settings.py
+++ backend/settings.py
@@ -31,23 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 EXCHANGE_RATE_API_URL = os.getenv('EXCHANGE_RATE_API_URL')
 
-DEBUG = bool(os.getenv('DEBUG', default=0))
-
-
-if DEBUG: 
-    WEBSITE_URL = 'http://localhost:3000'
-else:
-    WEBSITE_URL = 'https://walletwave-079e0ff9a62d.herokuapp.com/'
-
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(',')
-
-AUTH_USER_MODEL = 'useraccount.User'
-
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
-    }
-}
+DEBUG = True 
 
 SITE_ID = 1
 
