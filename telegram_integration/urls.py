1be459cbea63ae51f1fea6339710c3a8a9d8e6a5
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-06-11 01:33:18 +0400

    will do auth manually

--- telegram_integration/urls.py
+++ telegram_integration/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('telegram_auth/', telegram_auth, name='telegram_auth'),
 ]
--- useraccount/models.py
+++ useraccount/models.py
@@ -42,6 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255, blank=True, null=True)
     currency = models.CharField(max_length=20, choices=TransactionCurrencyUser.choices, default=TransactionCurrencyUser.USD)
     openai_key = models.CharField(max_length=1000, blank=True, null=True)
+    telegram_user_id = models.CharField(max_length=1000, blank=True, null=True)
 
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
